c91f108aee6f3d0ea4c00232e8a6a25cadf083fe
Harmeetrai <harmeetrai18@gmail.com>
2023-12-28 10:23:28 -0800

    Initial Commit

---
+++ iMusic/iMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMusicApp.swift
+//  iMusic
+//
+//  Created by Harmeet Rai on 2023-12-28.
+//
+
+import SwiftUI
+
+@main
+struct iMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
